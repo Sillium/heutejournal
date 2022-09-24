e3841cff99004333474fe82acea0eacee4d1036c
Sillium <stoerk@gmx.de>
2022-09-24 21:45:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import render_template
 app = Flask(__name__)
 
 # base_url = "https://www.tvspielfilm.de/tv-programm/sendungen/zdf,ZDF.html"
-base_url = "https://www.tvspielfilm.de/tv-programm/sendungen/?time=day&channel=ZDF"
+base_url = "https://www.tvspielfilm.de/tv-programm/sendungen/?time=primetime&channel=ZDF"
 
 def get_when(search_term):
     url = base_url
